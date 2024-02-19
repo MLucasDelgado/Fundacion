0f33c9311cd4cd89e24e22658f1ad6e32e475228
MLucasDelgado <lucasdelgado1011@gmail.com>
2024-02-19 17:16:37 -0300

    agregado de footer

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Footer.module.css'
+import Image from 'next/image'
+
+function Footer() {
+  return (
+    <section className={styles.seccion}>
+        <h1>hola</h1>
+    </section>
+  )
+}
+
+export default Footer
