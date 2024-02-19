d3e98c5f7a87687cdcacbdbf8abec3f8e37a25d7
MLucasDelgado <lucasdelgado1011@gmail.com>
2024-02-19 17:15:54 -0300

    cambios en el nav

--- components/navBar/NavBar.jsx
+++ components/navBar/NavBar.jsx
@@ -1,11 +1,15 @@
 import Link from 'next/link'
+import style from './NavBar.module.css'
 
 function NavBar() {
   return (
     <div>
-        <nav>
-            <Link href='#hola'>Inicio</Link>
-            <Link href=''>Nosotros</Link>
+        <nav className={style.nav}>
+            <Link className={style.enlaces} href='#hola'>Inicio</Link>
+            <Link className={style.enlaces} href='#mision'>Objetivos</Link>
+            <Link className={style.enlaces} href='#taller'>Qué hacemos</Link>
+            <Link className={style.enlaces} href=''>Donacion</Link>
+            <Link className={style.enlaces} href=''>Contacto</Link>
         </nav>
     </div>
   )
