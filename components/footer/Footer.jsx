320815a62f0cf52eb815899130be084f8a8b67a0
MLucasDelgado <lucasdelgado1011@gmail.com>
2024-02-26 19:08:30 -0300

    estrucutra de footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
 import styles from './Footer.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function Footer() {
   return (
     <section className={styles.seccion}>
-        <h1>hola</h1>
+      <section className={styles.contenedor}>
+        <Image
+          src='/ubi.png'
+          className={styles.logo1}
+          width={30}
+          height={30}
+        />
+        <h3>
+          <a className={styles.link} href="https://www.google.com/maps/search/?api=1&query=Barrio+San+Expedito+calle+Batalla+de+Salta+y+Los+Lirios" target="_blank">Barrio San Expedito</a>
+        </h3>
+      </section>
+
+      <section className={styles.contenedor}>
+        <Image
+          src='/telefono.png'
+          className={styles.logo1}
+          width={30}
+          height={30}
+        />
+        <Link href="tel:3815152609" className={styles.link}>
+          <h3 className={styles.titulo}>
+            3815152609
+          </h3>
+        </Link>
+
+      </section>
+
+      <section className={styles.contenedor}>
+        <Image
+          src='/email.png'
+          className={styles.logo1}
+          width={30}
+          height={30}
+        />
+        <Link className={styles.link} href='mailto:fundacionfyd@gmail.com'>
+          <h3 className={styles.titulo}>
+            fundacionfyd@gmail.com
+          </h3>
+        </Link>
+      </section>
     </section>
   )
 }
