8e1c72dc18c574feb8e52bdee1af1644e2a0081d
MLucasDelgado <lucasdelgado1011@gmail.com>
2024-03-04 18:17:05 -0300

    sacamos navbar

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import NavBar from '@/components/navBar/NavBar'
 import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
+        
         {children}
         <Footer />
         </body>
