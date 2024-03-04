dfab25690106344cdbad3d19e3e8cd3df38f0d4d
MLucasDelgado <lucasdelgado1011@gmail.com>
2024-03-04 18:17:24 -0300

    cambio de seccion

--- app/page.js
+++ app/page.js
@@ -1,54 +1,134 @@
-
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './page.module.css'
 
 export default function Home() {
-
   return (
     <main className={styles.main}>
-      <section className={styles.seccion} id='inicio' >
-        <article className={styles.articulo}>
-          <h2 className={styles.titulo}>Fundación</h2>
-          <h4 className={styles.titulo1}>Familia y Discapacidad</h4>
-          <p className={styles.parrafo}>Nuestro objetivo social es el mejoramiento de la calidad de vida de las personas con discapacidad, así como el fomento de la inclusión y la igualdad de oportunidades para este colectivo. Para lograrlo, nos comprometemos a llevar a cabo una amplia variedad de iniciativas en obras de bien público y proyectos de carácter social, cultural, educativo, productivo y programas de salud.</p>
-          <p className={styles.parrafo}>
-            Además, buscamos sensibilizar a la comunidad sobre las necesidades y desafíos que enfrentan las personas con discapacidad, promoviendo la solidaridad y la colaboración en la construcción de un mundo más inclusivo y equitativo para todos.
-          </p>
-        </article>
-        <div className={styles.imagenContenedor}>
-          <Image
-            src='/inicio.jpeg'
-            className={styles.imagen}
-            width={1000}
-            height={230}
-          />
+      <section className={styles.contenedorPrincipal}>
+        {/* --------------------- SECCION DEL NAVBAR --------------------- */}
+        <div className={styles.contenedor}>
+          <nav className={styles.nav}>
+            <div className={styles.logo}>
+              <Image
+                src='/logo2.png'
+                width={70}
+                height={70} 
+                alt='logo'/>
+              <Link className={styles.enlaces} style={{ textAlign: 'center' }} href='#hola'>
+                <p>Familia y discapacidad</p>
+              </Link>
+            </div>
+            <div className={styles.links}>
+              <Link className={styles.enlaces} href='#nosotros'>Nosotros</Link>
+              <Link className={styles.enlaces} href='#mision'>Objetivos</Link>
+              <Link className={styles.enlaces} href='#taller'>Talleres</Link>
+              <Link className={styles.enlaces} href='#donación'>Donación</Link>
+            </div>
+          </nav>
         </div>
+
+        {/* --------------------- SECCION DE LA PORTADA --------------------- */}
+
+        <section className={styles.contenedorPortada}>
+          <div className={styles.gradientOverlay}></div>
+          <section className={styles.contentPortada}>
+            <h1 className={styles.tituloPortada}>FAMILIA Y DISCAPACIDAD</h1>
+            <p className={styles.parrafoPortada}>Trabajar en la mejora de la calidad de vida de las personas con discapacidad es un compromiso que abarca múltiples aspectos de su bienestar y su integración en la sociedad.</p>
+            <button className={styles.botonPortada}>Conócenos</button>
+          </section>
+          <Image
+            className={styles.portadaImagen}
+            src='/portada.jpg'
+            width={2000}
+            height={500}
+            alt='portada-pagina'
+          />
+        </section>
       </section>
 
-      <div id='nosotros' className={styles.divContenedor}>
-        <h1 className={styles.tit}>Se <span style={{fontWeight: '700'}}>parte</span> del Cambio</h1>
+      {/* --------------------- SECCION DE NOSOTROS --------------------- */}
+
+      <section id='nosotros'  className={styles.contenedorSomos}>
+        <h2 className={styles.titulosSomos}>Nuestra <span style={{ fontWeight: 'normal' }}>organización</span> </h2>
+        <p style={{marginBottom: '5rem'}}>Conoce acerca de nuestros valores.</p>
+        <section className={styles.sectionNosotros}>
+          <section className={styles.card}>
+            <Image
+              className={styles.imagenSomos}
+              src='/logo2.png'
+              width={500}
+              height={500}
+              alt='imagen-somos'
+            />
+            <article className={styles.cardBody}>
+            <h4 className={styles.titulo1}>Familia y Discapacidad</h4>
+              <p className={styles.cardInfo}>
+              Nuestro objetivo social es el mejoramiento de la calidad de vida de las personas con discapacidad, así como el fomento de la inclusión y la igualdad de oportunidades para este colectivo.
+              </p>
+            </article>
+          </section>
+
+          <section className={styles.card}>
+            <Image
+              className={styles.imagenSomos}
+              src='/logo2.png'
+              width={500}
+              height={500}
+              alt='imagen-somos'
+            />
+            <article className={styles.cardBody}>
+            <h4 className={styles.titulo1}>Familia y Discapacidad</h4>
+              <p className={styles.cardInfo}>
+              Para lograr nuesto objetivo, nos comprometemos a llevar a cabo una amplia variedad de iniciativas en obras de bien público y proyectos de carácter social, cultural, educativo, productivo y programas de salud.
+              </p>
+            </article>
+          </section>
+
+          <section className={styles.card}>
+            <Image
+              className={styles.imagenSomos}
+              src='/logo2.png'
+              width={500}
+              height={500}
+              alt='imagen-somos'
+            />
+            <article className={styles.cardBody}>
+            <h4 className={styles.titulo1}>Familia y Discapacidad</h4>
+              <p className={styles.cardInfo}>
+              Buscamos sensibilizar a la comunidad sobre las necesidades y desafíos que enfrentan las personas con discapacidad, promoviendo la solidaridad y la colaboración en la construcción de un mundo más inclusivo y equitativo para todos.
+              </p>
+            </article>
+          </section>
+        </section>
+      </section>
+
+      {/* --------------------- IMAGENES DE NOSOTROS --------------------- */}
+
+      <div className={styles.divContenedor}>
+        <h1 className={styles.tit}>Se <span style={{ fontWeight: '700' }}>parte</span> del Cambio</h1>
         <p className={styles.parraf}>Una organización que busca la igualdad en la sociedad.</p>
       </div>
 
       <section className={styles.niños}>
         <div className={styles.flexContainer}>
           <div className={styles.centeredVertical}>
-            <Image src='/inicio.jpeg' className={styles.imagenUno} width={2000} height={2000} />
+            <Image src='/inicio.jpeg' className={styles.imagenUno} width={2000} height={2000} alt='niños' />
           </div>
           <div className={styles.fullHeight}>
-            <Image src='/inicio.jpeg' className={styles.imagenDos} width={2000} height={2000} />
+            <Image src='/inicio.jpeg' className={styles.imagenDos} width={2000} height={2000} alt='niños' />
           </div>
           <section className={styles.dosImagenes}>
-            <Image src='/inicio.jpeg' className={styles.imagenTres} width={2000} height={2000} />
-            <Image src='/inicio.jpeg' className={styles.imagenCuatro} width={2000} height={2000} />
+            <Image src='/inicio.jpeg' className={styles.imagenTres} width={2000} height={2000} alt='niños' />
+            <Image src='/inicio.jpeg' className={styles.imagenCuatro} width={2000} height={2000} alt='niños' />
           </section>
         </div>
       </section>
 
 
 
-      {/* ---------------- SECCION DE OBJETIVOS ---------------- */}
+      {/* --------------------- SECCION DE OBJETIVOS --------------------- */}
+
       <section id='mision' className={styles.contenedorMaestro}>
         <h3 className={styles.textoPri}>Trayendo <span style={{ color: 'black', fontWeight: '500' }}>sonrisas</span> a quienes más lo necesitan.</h3>
         <p className={styles.parrafoPri}>A través de nuestros objetivos.</p>
@@ -60,6 +140,7 @@ export default function Home() {
                 className={styles.imagenes}
                 width={1000}
                 height={1000}
+                alt='mision'
               />
             </div>
             <h3 className={styles.encabezado}>Misión</h3>
@@ -76,6 +157,7 @@ export default function Home() {
                 className={styles.imagenes}
                 width={1000}
                 height={1000}
+                alt='vision'
               />
             </div>
             <h3 className={styles.encabezado}>Visión</h3>
@@ -92,6 +174,7 @@ export default function Home() {
                 className={styles.imagenes}
                 width={1000}
                 height={1000}
+                alt='valores'
               />
             </div>
             <h3 className={styles.encabezado}>Valores</h3>
@@ -135,6 +218,7 @@ export default function Home() {
                 className={styles.logo1}
                 width={30}
                 height={30}
+                alt='danza-taller'
               />
               <h3 className={styles.tituloTaller}>Danza</h3>
               <p style={{ paddingBottom: '.8rem' }}>Unirse al taller de danza es una oportunidad única para explorar el mundo del movimiento y la expresión corporal de una manera inclusiva y enriquecedora.</p>
@@ -147,6 +231,7 @@ export default function Home() {
                 className={styles.logo1}
                 width={30}
                 height={30}
+                alt='Karate-taller'
               />
               <h3 className={styles.tituloTaller}>Karate</h3>
               <p style={{ paddingBottom: '.8rem' }}>En este taller, cada participante tiene la oportunidad de explorar su potencial, superar desafíos y alcanzar metas que parecían inalcanzables.</p>
@@ -161,6 +246,7 @@ export default function Home() {
                 className={styles.logo1}
                 width={35}
                 height={35}
+                alt='Arte-taller'
               />
               <h3 className={styles.tituloTaller}>Arte</h3>
               <p style={{ paddingBottom: '.8rem' }}>A través de la pintura, se despiertan los sentidos y se libera la imaginación, permitiendo a cada participante explorar su mundo interior y plasmarlo en lienzo.</p>
@@ -173,6 +259,7 @@ export default function Home() {
                 className={styles.logo1}
                 width={30}
                 height={30}
+                alt='Reciclado-taller'
               />
               <h3 className={styles.tituloTaller}>Reciclado</h3>
               <p style={{ paddingBottom: '.8rem' }}>A través del reciclado, se transforman materiales aparentemente insignificantes en piezas únicas y funcionales, dando una nueva vida a objetos que de otro modo podrían haber sido descartados</p>
@@ -195,6 +282,7 @@ export default function Home() {
               className={styles.logo1}
               width={50}
               height={50}
+              alt='Recolecta-donacion'
             />
             <h3>
               Recolecta
@@ -210,6 +298,7 @@ export default function Home() {
               className={styles.logo1}
               width={50}
               height={50}
+              alt='Eventos-donacion'
             />
             <h3>
               Eventos
@@ -225,6 +314,7 @@ export default function Home() {
               className={styles.logo1}
               width={50}
               height={50}
+              alt='Condiciones-donacion'
             />
             <h3>Condiciones</h3>
             <p style={{ textAlign: 'center', padding: '0 2rem' }}>
